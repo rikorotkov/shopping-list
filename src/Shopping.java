e886299689ac120cd3489d812a1a67919405dd11
Roman <floda935@gmail.com>
2024-09-06 10:21:43 +0300

    Выполнено дополнительное первое и второе задания

--- src/Shopping.java
+++ src/Shopping.java
@@ -19,34 +19,49 @@ public class Shopping {
 
             int actionNumber = scanner.nextInt();
 
-            if (actionNumber == 1) {
-                if (productCount < 8) {
-                    boolean isDuplicate = false;
+            // Создание нового массива при переполнении
+            if (productCount == shoppingList.length) {
+                String[] oldShoppingList = new String[productCount];
 
-                    System.out.println("Введите название товара для добавленяи в список:");
-                    String product = scanner.next();
-
-                    // проверка на дубликаты
-                    for (int i = 0; i < shoppingList.length; i++) {
-                        if (product.equals(shoppingList[i])) {
-                            isDuplicate = true;
-                            break;
-                        }
-                    }
-
-                    if (!isDuplicate) {
-                        shoppingList[productCount] = product;
-                        System.out.println("Товар " + product + " добавлен в список под номером " + (productCount + 1));
-                        productCount++;
-                    } else {
-                        System.out.println("Данный товар уже есть в списке покупок.");
-                    }
-
-                } else {
-                    System.out.println("Список товаров переполнен!");
+                for (int i = 0; i < shoppingList.length; i++) {
+                    oldShoppingList[i] = shoppingList[i];
                 }
 
+                shoppingList = new String[productCount + 1];
+
+                for (int i = 0; i < oldShoppingList.length; i++) {
+                    shoppingList[i] = oldShoppingList[i];
+                }
+            }
+
+            // Добавление товара в список
+            if (actionNumber == 1) {
+
+                boolean isDuplicate = false;
+
+                System.out.println("Введите название товара для добавленяи в список:");
+                String product = scanner.next();
+
+                // проверка на дубликаты
+                for (int i = 0; i < shoppingList.length; i++) {
+                    if (product.equals(shoppingList[i])) {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+
+                if (!isDuplicate) {
+                    shoppingList[productCount] = product;
+                    System.out.println("Товар " + product + " добавлен в список под номером " + (productCount + 1));
+                    productCount++;
+                } else {
+                    System.out.println("Данный товар уже есть в списке покупок.");
+                }
+
+                // Показать список товаров
             } else if (actionNumber == 2) {
+                System.out.println("Список товаров:");
+
                 if (productCount != 0) {
                     for (int i = 0; i < shoppingList.length; i++) {
                         if (shoppingList[i] == null) {
@@ -59,12 +74,14 @@ public class Shopping {
                     System.out.println("Список товаров пуст. Добавьте товары в список.");
                 }
 
+                // Очистить список товаров
             } else if (actionNumber == 3) {
                 for (int i = 0; i < shoppingList.length; i++) {
                     shoppingList[i] = null;
                 }
                 System.out.println("Список товаров очищен.");
 
+                // Выход из приложения
             } else if (actionNumber == 4) {
                 break;
             } else {
